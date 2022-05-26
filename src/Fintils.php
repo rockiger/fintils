eb89818384836769d89f1a89d7033b30c0874c30
rockinger <marco.laspe@zippeo.io>
2022-05-26 08:40:28 +0200

    Create an arrray of Rs and function to create a that calculates the QuantDare correlation dq.

--- src/Fintils.php
+++ src/Fintils.php
@@ -2,6 +2,8 @@
 
 namespace Rockiger\Fintils;
 
+use ErrorException;
+
 class Fintils {
 
   /**
@@ -17,5 +19,55 @@ class Fintils {
       return 0.0;
     }
     return ($P_t / $P_t_minus_1) - 1;
+  }
+
+
+  /**
+   * Converts an array of prices to an array of returns
+   * 
+   * @param float[] $Ps 
+   * @return float[]
+   */
+  public static function Rs(array $Ps): array {
+    $Rs = [];
+    for ($i = 0; $i < count($Ps); $i++) {
+      if ($i == 0) {
+        $Rs[$i] = 0.0;
+      } else {
+        $Rs[$i] = self::R($Ps[$i], $Ps[$i - 1]);
+      }
+    }
+    return $Rs;
+  }
+
+
+  /**
+   * QuantDare correlation
+   * A measure of how much two random variables change together without the general trend
+   * of the mean.
+   * https://quantdare.com/correlation-prices-returns/
+   * 
+   *           ∑xᵢyᵢ
+   * dqxy = -----------
+   *         √∑xᵢ²∑yᵢ²
+   * 
+   * @param array $X 
+   * @param array $Y 
+   * @return float 
+   */
+  public static function dq(array $X, array $Y): float {
+
+    if (count($X) !== count($Y)) {
+      throw new ErrorException('X and Y must have the same number of elements.');
+    }
+    if (count($X) < 2) {
+      throw new ErrorException('X and Y must have at least two elements.');
+    }
+
+    $∑XY = array_sum(array_map(fn (float $x, float $y) => $x * $y, $X, $Y));
+    $∑X² = array_sum(array_map(fn (float $x) => $x * $x, $X));
+    $∑Y² = array_sum(array_map(fn (float $y) => $y * $y, $Y));
+    $√ = sqrt($∑X² * $∑Y²);
+    return $∑XY / $√;
   }
 }
--- tests/FintilsTest.php
+++ tests/FintilsTest.php
@@ -13,4 +13,39 @@ final class FintilsTest extends TestCase {
     $this->assertEquals(0.0, Fintils::R(100, 100));
     $this->assertEquals(0.0, Fintils::R(100));
   }
+
+  public function testRs(): void {
+    $this->assertEquals(
+      [0, 0.1, 0.0454545454545454, -0.0869565217391305, -0.0476190476190477],
+      Fintils::Rs([100, 110, 115, 105, 100])
+    );
+    $this->assertEquals(
+      [],
+      Fintils::Rs([])
+    );
+
+    $this->assertEquals(
+      [0.0],
+      Fintils::Rs([100])
+    );
+  }
+
+  public function testDq(): void {
+    $this->assertEquals(
+      1.0,
+      Fintils::dq([0, 0.1, 0.0454545454545454, -0.0869565217391305, -0.0476190476190477], [0, 0.1, 0.0454545454545454, -0.0869565217391305, -0.0476190476190477])
+    );
+    $this->assertEquals(
+      -1.0,
+      Fintils::dq(
+        [0, -0.1, -0.0454545454545454, 0.0869565217391305, 0.0476190476190477],
+        [0, 0.1, 0.0454545454545454, -0.0869565217391305, -0.0476190476190477]
+      )
+    );
+    $this->expectExceptionMessage('X and Y must have the same number of elements.');
+    Fintils::dq([0, 0.1], [0]);
+
+    $this->expectExceptionMessage('X and Y must have at least two elements.');
+    Fintils::dq([0], [0]);
+  }
 }
